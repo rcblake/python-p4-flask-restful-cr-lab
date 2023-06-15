a969ccde0bf40c3c9168774ea87f98214821edee
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-15 16:50:50 -0700

    model with decimal.

--- server/models.py
+++ server/models.py
@@ -1,9 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import DECIMAL
 
 db = SQLAlchemy()
 
+
 class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
+    __tablename__ = "plants"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.DECIMAL(10, 2))
